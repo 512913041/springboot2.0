5f2b14ef11c91114199095309cd20a2e731925f7
LYNSH <512913041@qq.com>
2018-09-09 16:29:27 +0800

    hzdsn-server

---
+++ src/main/java/com/hzdsn/App.java
@@ -0,0 +1,18 @@
+package com.hzdsn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
